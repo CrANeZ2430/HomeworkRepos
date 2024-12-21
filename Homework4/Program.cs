da59c9e4db72440c30fb87892a5794332e8ed644
CrnZ1675 <crn.alexei@gmail.com>
2024-12-21 19:16:23 +0200

    Small changes

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -3,7 +3,7 @@
 var customList = new CustomList<User>()
 {
     new User(890753, "Vadym", new DateTime(2004, 7, 2)),
-    new User(270532, "Oleksii", new DateTime(2002, 6, 27)),
+    new User(270532, "Oleksiy", new DateTime(2002, 6, 27)),
     new User(026549, "Walter", new DateTime(1965, 2, 15))
 };
 
@@ -13,15 +13,12 @@ customList.Add(new User(368284, "Mike", new DateTime(1954, 4, 21)));
 customList.Add(new User(672515, "Jimmy", new DateTime(1967, 5, 30)));
 
 //random linq method
-var userWithSpecialId = customList.FirstOrDefault(x => x.Id == 672515).Name;
-Console.WriteLine(userWithSpecialId);
+Console.WriteLine(customList.FirstOrDefault(x => x.Id == 672515)?.Name);
 Console.WriteLine();
 
 //filtration
-var usersOlderThanSixty = customList.Where(x => (DateTime.Now - x.Birthday).TotalDays / 365 > 40).Select(x => x.Name);
-Console.WriteLine(string.Join(", ", usersOlderThanSixty));
+Console.WriteLine(string.Join(", ", customList.Where(x => (DateTime.Now - x.Birthday).TotalDays / 365 > 40).Select(x => x.Name)));
 Console.WriteLine();
 
 //sorting
-var listToDisplay = customList.OrderBy(x => x.Id).Select(x => x.Name);
-Console.WriteLine(string.Join(", ", listToDisplay));
+Console.WriteLine(string.Join(", ", customList.OrderBy(x => x.Id).Select(x => x.Name)));
