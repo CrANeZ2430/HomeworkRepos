6e2dfc5190493cf3261a9733e369c40dd1b7848b
CrANeZ2430 <crn.alexei@gmail.com>
2024-12-25 00:18:25 +0200

    Finished homework 5 (and small change to homework 4)

---
+++ Homework5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+var regexForBinary = new Regex("[0,1]$");
+var regexForDivisionByTwo = new Regex("[0,2,4,6,8]$");
+int binaryNumberToEnter;
+while (true)
+{
+    Console.Write("Enter a binary number: ");
+    var input = Console.ReadLine()!;
+
+    if (int.TryParse(input, out binaryNumberToEnter) && regexForBinary.IsMatch(input))
+        break;
+}
+
+var deciamlNumber = ConvertBinaryToDecimal(binaryNumberToEnter);
+Console.WriteLine($"Your decimal number: {deciamlNumber}");
+Console.WriteLine($"Ability to divide by 2: {regexForDivisionByTwo.IsMatch(deciamlNumber.ToString())}");
+
+int ConvertBinaryToDecimal(int binaryNumber)
+{
+    var binaryNumberString = binaryNumber.ToString();
+    var attempts = binaryNumber.ToString().Length;
+    var sum = 0;
+    for (var i = 0; i < attempts; i++)
+        sum += int.Parse(binaryNumberString[attempts - 1 - i].ToString()) * (int)Math.Pow(2, i);
+
+    return sum;
+}
