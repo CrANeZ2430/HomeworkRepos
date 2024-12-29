a54a8de1a480cda07819455ec39ef087ddf70a59
CrANeZ2430 <crn.alexei@gmail.com>
2024-12-29 16:50:52 +0200

    Homework 6 (thread safety)

---
+++ Homework6/Clipboard.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework6;
+
+public static class Clipboard
+{
+    private static readonly Queue<string> s_clipboard = new();
+    public static object LockerObject { get; } = new();
+
+    public static void Push(string data)
+    {
+        lock (LockerObject) s_clipboard.Enqueue(data);
+    }
+
+    public static string Get()
+    {
+        lock (LockerObject) return s_clipboard.Dequeue();
+    }
+}
---
+++ Homework6/Program.cs
@@ -0,0 +1,14 @@
+﻿using Homework6;
+
+var pr = new Producer();
+var cn = new Consumer();
+
+while (true)
+{
+    pr.PushData();
+    cn.ProcessData();
+    Console.WriteLine("Process more data or exit");
+    var str = Console.ReadLine();
+    if (str == "exit")
+        break;
+}
