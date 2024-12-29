83f9388b521995aca51ed627b47bc908ed288123
CrANeZ2430 <crn.alexei@gmail.com>
2024-12-29 16:50:52 +0200

    Homework 6 (thread safety)

---
+++ Homework6/Consumer.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework6;
+
+public class Consumer
+{
+    public void ProcessData()
+    {
+        lock (Clipboard.LockerObject)
+        {
+            if (!File.Exists(@"C:\Temp\data.txt"))
+                File.Create(@"C:\Temp\data.txt").Dispose();
+
+            var data = Clipboard.Get();
+            File.AppendAllText(@"C:\Temp\data.txt", data + Environment.NewLine);
+        }
+    }
+}
---
+++ Homework6/Producer.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace Homework6;
+
+public class Producer
+{
+    private string[] _words = [ "World", "Trees", "Houses",
+                                "People", "Forest", "Morning",
+                                "Cars", "Cats", "Dogs", "Plants" ];
+
+    private string GenerateData()
+    {
+        var sb = new StringBuilder();
+        for (var i = 0; i < 5; i++)
+            sb.Append(($"{_words[Random.Shared.Next(_words.Length)]} "));
+        return sb.ToString();
+    }
+
+    public void PushData()
+    {
+        lock (Clipboard.LockerObject)
+            Clipboard.Push(GenerateData());
+    }
+}
