9fe97196ce261efc15e810b863abec50e2cd0660
CrANeZ2430 <crn.alexei@gmail.com>
2025-01-12 19:44:36 +0200

    7 homework (async/await)

---
+++ Homework7/FileManagement.cs
@@ -0,0 +1,23 @@
+﻿namespace Homework7;
+
+public static class FileManagement
+{
+    public static void FileManagementSync(int loopNumber)
+    {
+        var path = @"C:\Temp\number.txt";
+        File.Create(path).Close();
+        File.AppendAllText(path, loopNumber.ToString());
+        File.Delete(path);
+    }
+
+    public static async Task FileManagementAsync(int loopNumber)
+    {
+        await Task.Run(() =>
+        {
+            var path = @"C:\Temp\number.txt";
+            File.Create(path).Close();
+            File.AppendAllText(path, loopNumber.ToString());
+            File.Delete(path);
+        });
+    }
+}
---
+++ Homework7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+using Homework7;
+
+var sw = new Stopwatch();
+
+// sync method
+sw.Start();
+for (var i = 0; i < 100; i++)
+{
+    FileManagement.FileManagementSync(i);
+}
+sw.Stop();
+Console.WriteLine($"Sync: {sw.ElapsedMilliseconds} ms");
+sw.Reset();
+
+// async method
+sw.Start();
+for (var i = 0; i < 100; i++)
+{
+    await FileManagement.FileManagementAsync(i);
+}
+sw.Stop();
+Console.WriteLine($"Async: {sw.ElapsedMilliseconds} ms");
